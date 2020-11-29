e8626d8896e6f807a6e0139ddc703e8b28bb12be
Aaron Ivie <aivie@byui.edu>
2020-11-28 22:30:12 -0700

    Initial Commit

---
+++ calculator2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator2
+//
+//  Created by Aaron Ivie on 11/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
